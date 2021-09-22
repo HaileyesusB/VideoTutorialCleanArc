dbe1883297b260d0969472223154032116d4dd96
Haileyesus B <hbelew@excellerentsolutions.com>
2021-09-22 13:01:25 +0300

    Domain and Presentaion

---
+++ Clean.Arc.Solution.UI.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Clean.Arc.Solution.UI.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
