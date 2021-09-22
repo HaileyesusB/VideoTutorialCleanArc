7136e9167acbff11a7b5e40b7803e5c7a173e141
Haileyesus B <hbelew@excellerentsolutions.com>
2021-09-22 13:22:26 +0300

    Domain.Model Class Updated

---
+++ Clean.Arc.Domain/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clean.Arc.Domain.Models
+{
+    public class Course
+    {
+        public int ID { get; set; }
+        public string Name { get; set;}
+
+        public string description { get; set;}
+
+        public string ImageURL { get; set;}
+    }
+}
