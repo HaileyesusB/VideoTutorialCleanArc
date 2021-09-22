c925ee1aa6ffc6aa8827bb5d61543b6306c48a34
Haileyesus B <hbelew@excellerentsolutions.com>
2021-09-22 14:14:10 +0300

    DbContext

---
+++ Clean.Arc.Infra.Data/Context/VideoCourseDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Clean.Arc.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clean.Arc.Infra.Data.Context
+{
+    public class VideoCourseDbContext: DbContext 
+    {
+
+        public VideoCourseDbContext(DbContextOptions options): base(options) { }
+    
+         public DbSet<Course> Courses { get; set; }
+    }
+
+}
