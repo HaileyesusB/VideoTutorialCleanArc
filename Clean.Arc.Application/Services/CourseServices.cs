232c35f610307ca278b4571e3944cf26c7af38d2
Haileyesus B <hbelew@excellerentsolutions.com>
2021-09-23 08:37:07 +0300

    Application.Service is created

--- Clean.Arc.Application/Services/CourseServices.cs
+++ Clean.Arc.Application/Services/CourseServices.cs
@@ -1,8 +1,6 @@
 ﻿using Clean.Arc.Application.Interfaces;
 using Clean.Arc.Application.ViewModels;
 using Clean.Arc.Domain.Interfaces;
-using System;
-using System.Collections.Generic;
 
 namespace Clean.Arc.Application.Services
 {
